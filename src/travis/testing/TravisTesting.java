3b6c8d4891eadb3393b1285edd9820a83a71b71b
LIANG <LIANG@DESKTOP-BD76IC7>
2018-12-06 20:54:24 +0800

    aaaa

---
+++ src/travis/testing/TravisTesting.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package travis.testing;
+
+/**
+ *
+ * @author LIANG
+ */
+public class TravisTesting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
